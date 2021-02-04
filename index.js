eb3532f5f119594b73d75415a23d24d86aee6af5
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2021-02-03 23:31:45 -0600

    2021-02-03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { requestTime, logger } from "./middlewares.js";
+import serverRoutes from "./routes/servers.js";
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
@@ -16,12 +17,14 @@ app.use(express.static(path.resolve(__dirname, "static")));
 app.use(requestTime);
 app.use(logger);
 
+app.use(serverRoutes);
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Main Page", active: "main" });
 });
 
 app.get("/features", (req, res) => {
-  res.render("features", { title: "Features Page", active:'features' });
+  res.render("features", { title: "Features Page", active: "features" });
 });
 
 // app.get("/", (req, res) => {
